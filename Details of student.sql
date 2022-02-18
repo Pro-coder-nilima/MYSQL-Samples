913117cbadbf9692b1eaf2460281513d1a88ce6d
Nilima Misala <99179769+Pro-coder-nilima@users.noreply.github.com>
2022-02-18 13:20:54 +0530

    Add files via upload

---
+++ Details of student.sql
@@ -0,0 +1,28 @@
+
+create table student(
+stud_id int,
+stud_name varchar(20),
+stud_age int,
+stud_gender varchar(20)
+);
+
+insert into student values(
+1,'siri', 20,'female'
+);
+
+insert into student values(
+2,'sruthi', 21, 'feamle'
+);
+insert into student values(
+3,'shiva', 24,'male'
+);
+
+insert into student values(
+4,'arun', 20,'male'
+);
+insert into  student values(
+5, 'akhila',19,'female'
+);
+
+select *from student
+
